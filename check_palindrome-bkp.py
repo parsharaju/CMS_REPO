bb12274bef593188ff740461a899a313605bf185
Raju Parsha <raju.parsha@redpincompany.com>
2024-01-23 20:02:56 +0530

    Tue, Jan 23, 2024  8:02:55 PM

---
+++ check_palindrome-bkp.py
@@ -0,0 +1,12 @@
+
+
+def check_palimdrone(str):
+    reverse_str = str[::-1]
+
+    if reverse_str == str:
+        return True
+    else:
+        return False
+
+str = input("Enter the string to check for palindrome: ")
+print(check_palimdrone(str))
